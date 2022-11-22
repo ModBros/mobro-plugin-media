ebc96621cf8aac469bb5f05c83fa50e4ada3edfd
Manfred Graf <manfred.graf91@gmail.com>
2022-11-23 00:03:17 +0100

    chore(project): initialized project

---
+++ Media.cs
@@ -0,0 +1,23 @@
+﻿using MoBro.Plugin.SDK;
+
+namespace MoBro.Plugin.Media;
+
+public sealed class Media : IMoBroPlugin
+{
+  private bool _paused;
+  private IMoBroService _service;
+
+  public void Init(IPluginSettings settings, IMoBroService service)
+  {
+    _service = service;
+
+  }
+
+  public void Pause() => _paused = true;
+
+  public void Resume() => _paused = false;
+
+  public void Dispose()
+  {
+  }
+}
