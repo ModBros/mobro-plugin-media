661e6dc5114e761f7a07f5c4ab27f4ca821b8109
Manfred Graf <manfred.graf91@gmail.com>
2023-03-15 00:07:46 +0100

    refactor: removed settings from audio actions

--- Handlers/AudioHandler.cs
+++ Handlers/AudioHandler.cs
@@ -20,31 +20,8 @@ public class AudioHandler : AbstractHandler
 
   public override IEnumerable<IAction> GetActions()
   {
-    yield return Action(Ids.Action.MVolumeUp)
-      .WithHandler(VolumeUp)
-      .WithSetting(b => b
-        .WithName("step_amount")
-        .WithLabel("Step width")
-        .OfTypeNumber()
-        .WithDefault(1)
-        .WithMin(1)
-        .WithMax(100)
-        .Build()
-      )
-      .Build();
-
-    yield return Action(Ids.Action.MVolumeDown)
-      .WithHandler(VolumeDown)
-      .WithSetting(b => b
-        .WithName("step_amount")
-        .WithLabel("Step width")
-        .OfTypeNumber()
-        .WithDefault(1)
-        .WithMin(1)
-        .WithMax(100)
-        .Build()
-      )
-      .Build();
+    yield return Action(Ids.Action.MVolumeUp).WithHandler(VolumeUp).Build();
+    yield return Action(Ids.Action.MVolumeDown).WithHandler(VolumeDown).Build();
   }
 
   public override async IAsyncEnumerable<MetricValue> GetMetricValues()
